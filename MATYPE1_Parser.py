fe394237ca8758e13ca796f58f816987b1adce50
Eric Corrigal <ejrcorrigal@gmail.com>
2023-03-26 21:34:55 -0600

    Added a method to mask the input number based on its bit position and number of bits.

--- MATYPE1_Parser.py
+++ MATYPE1_Parser.py
@@ -29,22 +29,25 @@ class MATYPE1_MASK:
         self._RO_BITS = 2
         self._RO_MASK = int('00000011', 2)
     
+    def mask_bits(self, number, number_bits, mask, bit_position, number_of_bits):
+        return (mask & number) >> (number_bits - bit_position - number_of_bits)
+
     def get_TS_GS(self, MATYPE_INT):
-        return (self._TS_GS_MASK & MATYPE_INT) >> (self._MATYPE_BITS-self._TS_GS_BIT_POSITION-self._TS_GS_BITS)
+        return self._mask_bits(MATYPE_INT, self._MATYPE_BITS, self._TS_GS_MASK, self._TS_GS_BIT_POSITION, self._TS_GS_BITS)
     
     def get_SIS_MIS(self, MATYPE_INT):
-        return (self._SIS_MIS_MASK & MATYPE_INT) >> (self._MATYPE_BITS-self._SIS_MIS_BIT_POSITION-self._SIS_MIS_BITS)
+        return self._mask_bits(MATYPE_INT, self._MATYPE_BITS, self._SIS_MIS_MASK, self._SIS_MIS_BIT_POSITION, self._SIS_MIS_BITS)
     
-    def get_CMM_ACM(self, MATYPE_INT):
-        return (self._CCM_ACM_MASK & MATYPE_INT) >> (self._MATYPE_BITS-self._CCM_ACM_BIT_POSITION-self._CCM_ACM_BITS)
+    def get_CCM_ACM(self, MATYPE_INT):
+        return self._mask_bits(MATYPE_INT, self._MATYPE_BITS, self._CCM_ACM_MASK, self._CCM_ACM_BIT_POSITION, self._CCM_ACM_BITS)
     
     def get_ISSYI(self, MATYPE_INT):
-        return (self._ISSYI_MASK & MATYPE_INT) >> (self._MATYPE_BITS-self._ISSYI_BIT_POSITION-self._ISSYI_BITS)
+        return self._mask_bits(MATYPE_INT, self._MATYPE_BITS, self._CCM_ACM_MASK, self._CCM_ACM_BIT_POSITION, self._CCM_ACM_BITS)
     
     def get_NPD(self, MATYPE_INT):
-        return (self._NPD_MASK & MATYPE_INT) >> (self._MATYPE_BITS-self._NPD_BIT_POSITION-self._NPD_BITS)
+        return self._mask_bits(MATYPE_INT, self._MATYPE_BITS, self._NPD_MASK, self._NPD_BIT_POSITION, self._NPD_BITS)
     
     def get_RO(self, MATYPE_INT):
-        return (self._RO_MASK & MATYPE_INT) >> (self._MATYPE_BITS-self._NPD_BIT_POSITION-self._NPD_BITS)
+        return self._mask_bits(MATYPE_INT, self._MATYPE_BITS, self._RO_MASK, self._RO_BIT_POSITION, self._RO_BITS)
     
     
